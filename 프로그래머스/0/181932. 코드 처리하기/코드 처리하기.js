3d2f0fa77d25454ad5190be24f0548748030f43a
Dev_mine <alsgp0877@naver.com>
2024-01-17 01:57:28 +0900

    Update 코드 처리하기.js

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -30,3 +30,26 @@ function solution(code) {
     }
     return answer;
 }
+//초기코드
+//만약 문자가 "1"이면 
+//mode는 0과 1이 있으며, idx를 0 부터 code의 길이 - 1 까지 1씩 키워나가면서 code[idx]의 값에 따라 다음과 같이 행동합니다. 
+// if(zCode[i] == '1'){
+//     if(zCode[i] != '1'){
+//         if(zCode[i]%2 != 0){//code[idx]가 "1"이 아니면 idx가 홀수일 때만
+//             ret.push(zCode[i]);//ret의 맨 뒤에 code[idx]를 추가
+//         }
+//     }else if(zCode[i] == '1'){
+//         //mode를 0에서 1으로 바꿉니다.
+//         zCode[i] = '1';
+//     }
+// }
+//if(zCode[i] == '0'){//mode가 0일 때
+//     if(zCode[i] != '1'){
+//        if(zCode[i]%2 == 0){//code[idx]가 "1"이 아니면 idx가 짝수일 때만
+//           ret.push(zCode[i]);//ret의 맨 뒤에 code[idx]를 추가
+//        }
+//     }else if(zCode[i] == '1'){//code[idx]가 "1"이면 
+//         //mode를 1에서 0으로 바꿉니다.
+//         zCode[i] = '0';
+//     }
+//}
